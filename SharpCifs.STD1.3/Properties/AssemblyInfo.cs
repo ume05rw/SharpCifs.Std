d7f7e928c3bb8706c6baf3dc577b2464ca52f24c
ume05rw <info@dobes.jp>
2017-05-10 12:00:21 +0900

    ソケットプール実装を追加。ただし、まだ既存ロジックに組み込んでいない。

--- SharpCifs.STD1.3/Properties/AssemblyInfo.cs
+++ SharpCifs.STD1.3/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.3.1")]
-[assembly: AssemblyFileVersion("0.2.3.1")]
+[assembly: AssemblyVersion("0.2.3.2")]
+[assembly: AssemblyFileVersion("0.2.3.2")]
