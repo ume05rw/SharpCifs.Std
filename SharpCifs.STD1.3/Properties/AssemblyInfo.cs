bf166d4f9557e4d75aa6f5eb31d6f00dd364f5f0
ume05rw <info@dobes.jp>
2017-09-23 07:58:48 +0900

    count up version.

--- SharpCifs.STD1.3/Properties/AssemblyInfo.cs
+++ SharpCifs.STD1.3/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.2.9")]
-[assembly: AssemblyFileVersion("0.2.9")]
+[assembly: AssemblyVersion("0.2.10")]
+[assembly: AssemblyFileVersion("0.2.10")]
